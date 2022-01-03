82448361a8bfabbf143339b146dae1fc7c14f8f6
Hassan-Erfani <erfani.mojtaba@hotmail.com>
2022-01-03 17:32:41 +0100

    recursion init

---
+++ src/recursion/DemoSN.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class DemoSN {
+    public static void main(String[] args) {
+
+        SocialNetwork sn = SocialNetwork.generateDemoNetwork();
+
+        //TODO 1: geben Sie alle User des Social Networks basierend
+        //auf Ihrer geschriebenen .toString() Methode aus
+
+        //TODO 2: probieren Sie die implementierte rekursive Methode
+        //der Klasse SocialNetwork für verschiedene Benutzer und
+        //Werte für maxDepth aus.
+
+    }
+}
